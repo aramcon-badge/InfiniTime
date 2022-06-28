a167889b7aedc67eccc01ff35205ca5da1110b7c
TomerAlkalay <alkalaytomer@gmail.com>
2022-06-28 12:46:24 -0400

    added test strings

--- src/displayapp/screens/WatchFaceTerminal.cpp
+++ src/displayapp/screens/WatchFaceTerminal.cpp
@@ -255,5 +255,5 @@ void WatchFaceTerminal::Refresh() {
   NRF_LOG_INFO("flags:  %s", ctf_solved);
 
   lv_label_set_text_fmt(label_ctf, 
-                          "[LVL ] #00FF00 %s#", ctf_solved);
+                          "[LVL ] #00FF00 %s TEST TEST TEST#", ctf_solved);
 }
